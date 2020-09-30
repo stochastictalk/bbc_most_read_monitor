7d23b39345873dbf254a3984e2dafe6deedd950a
Jerome <jerome.wynne18@ic.ac.uk>
2020-09-30 18:46:22 +0100

    Imported bs, requests, retrieved homepage HTML

--- bbc_scraper.py
+++ bbc_scraper.py
@@ -1,4 +1,9 @@
 # Script for scraping the BBC headlines
 
 import bs4 as bs
+import requests
 
+headline_src_url = 'https://bbc.co.uk/news')
+r = requests.get(headline_src_url)
+html = r.text
+soup = bs.BeautifulSoup(html, 'html.parser')
