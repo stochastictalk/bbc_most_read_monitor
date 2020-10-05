73c019605c1fa599d8f77cecedac5694d1cc5e62
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-05 22:16:53 +0100

    Added User-Agent header.

--- log_bbc_most_read.py
+++ log_bbc_most_read.py
@@ -8,7 +8,8 @@ time_of_request = datetime.now().strftime('%s') # time since epoch
 timeout = 15 # seconds
 
 # Throws an error if it times out (that's a good thing)
-r = requests.get(most_read_url, timeout=timeout)
+headers = {'User-Agent':'Mozilla/5.0'}
+r = requests.get(most_read_url, timeout=timeout, headers=headers)
 
 if 200 <= r.status_code and r.status_code < 300: # request successful
 	#file = open(dst_dir + '/bbc_most_read_' + time_of_request + '.html', 'w')
