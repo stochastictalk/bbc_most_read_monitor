c0b0a4d41c1b573c9c3e082fd04a90cc6d30b9e6
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-08 19:50:09 +0100

    Began Dash app

---
+++ app.py
@@ -0,0 +1,9 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as dhtml
+import plotly.express as px
+import pandas as pd
+from glob import glob
+
+# Gather headlines into a data frame
+
