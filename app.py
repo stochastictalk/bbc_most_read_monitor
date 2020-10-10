3ed72cc936c85eff207707b0a92644503bf68677
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-10 18:15:09 +0100

    Cleared app.py subsequent to switch to .json

--- app.py
+++ app.py
@@ -5,32 +5,9 @@ import plotly.express as px
 import pandas as pd
 from glob import glob
 
-def _parse(file_text: str):
-	'''
-		Receives contents of bbc_most_read_xxxxxxxxxx.txt as string.
-		Returns ordered list of headlines, most popular first.
-	'''
-	file_lines = file_text.split('\n')[:-1]
-	dct_of_headlines = {}
-	for j, line in enumerate(file_lines):
-		dct_of_headlines = ','.join(line.split(',')[1:-1]) # headline only
-	return(list_of_headlines)
-
 # Gather headlines into a data frame
-filepaths = glob('./data/bbc_most_read/*.txt')
+filepaths = glob('./data/bbc_most_read/*.json')
 
 # each file contains the 10 most read articles at that timestamp
 for fp in filepaths:
-	unixtime_threshold = 1602011668
-	file_unixtime = fp.split('_')[-1][:-4] # omit .txt
-	if int(file_unixtime) < unixtime_threshold:
-		continue
-
-	# Timestamp, 1, 2, 3, ..., 10
 	with open(fp, 'r') as file:
-		 = _parse(file.read())
-
-	else:
-		headlines_df = headlines_df.join(file_df)
-
-print(headlines_df.head(3))
