225093393b4be1638981d56f726c17a3445540b1
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-10 18:28:55 +0100

    Updated parser to work with .json records

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ import dash_html_components as dhtml
 import plotly.express as px
 import pandas as pd
 from glob import glob
+import json
 
 # Gather headlines into a data frame
 filepaths = glob('./data/bbc_most_read/*.json')
 
 # each file contains the 10 most read articles at that timestamp
+list_of_records = []
 for fp in filepaths:
 	with open(fp, 'r') as file:
+		file_str = file.read()
+	if len(file_str) == 0: # file is empty
+		continue
+	else:
+		list_of_records = list_of_records + json.loads(file_str)
+
+df = pd.DataFrame(list_of_records)
+
+print(df)
