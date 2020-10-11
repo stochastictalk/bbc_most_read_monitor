1e2bcae7b75a2a161fdc7151dd21c038d4d55d23
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-11 16:35:07 +0100

    Layout is cracked, now onto displaying the nearest headline preceding the input time

--- app1.py
+++ app1.py
@@ -34,42 +34,64 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Define app layout
 app.layout = dhtml.Div([
-	dhtml.Div([
 	dhtml.H4('BBC Most Read Visualiser V1.0',
 			 style={'font-weight':'bold'}),
+	dhtml.P('''
+			This app allows you to explore the most read articles on BBC News \
+			over time.
+	  		'''),
 
-	dcc.DatePickerSingle(
-		calendar_orientation='vertical',
-    	id='date-picker-single',
-    	date=date.fromtimestamp(df['TIMESTAMP'].max())
+	dhtml.Div([
+			dhtml.Div('Date',
+					  style={'font-weight':'bold',
+							 'width':'25%',
+							 'display':'inline-block'}),
+			dhtml.Div('Time of day',
+					  style={'font-weight':'bold',
+							 'width':'75%',
+							 'display':'inline-block'})
+			],
+		style={'width':'100%', 'display':'inline-block'}
 	),
 
-	dcc.Slider( # Time slider
-        id='time--slider',
-        min=0,
-        max=24,
-        value=18,
-        marks={
-			t:str(t).zfill(2)+':00' for t in range(0, 25, 2)
-		},
-        step=0.01,
-		included=False
-    ),
+	dhtml.Div([
+			dhtml.Div(
+	  			dcc.DatePickerSingle(
+	  				calendar_orientation='vertical',
+	  	    		id='date-picker-single',
+	  	    		date=date.fromtimestamp(df['TIMESTAMP'].max()),
+	  			),
+				style={'width':'25%', 'display':'inline-block'}
+			),
+			dhtml.Div(
+				dcc.Slider( # Time slider
+			        id='time--slider',
+			        min=0,
+			        max=24,
+			        value=18,
+			        marks={
+						t:str(t).zfill(2)+':00' for t in range(0, 25, 2)
+					},
+			        step=0.01,
+					included=False,
+				),
+				style={'width':'75%', 'display':'inline-block'}
+			)]),
 
 	dhtml.Br(),
 
-	dcc.Markdown(children='', id='my-output')],
-	style={'width': '48%', 'margin':'auto'})
-])
+	dcc.Markdown(children='', id='my-output')
+],
+style={'width': '48%', 'margin':'auto'})
 
 # Define callbacks
-'''
 @app.callback(
 	Output(component_id='my-output', component_property='children'),
-	[Input(component_id='time--slider', component_property='value')]
+	[Input(component_id='date-picker-single', component_property='date'),
+	 Input(component_id='time--slider', component_property='value')]
 )
-def update_output_div(input_t):
-	#return(input_t)
+def update_output_div(input_yyyy_mm_dd: str, input_hour: float):
+	return(input_yyyy_mm_dd + str(input_hour))
 	# Retrieve headlines for time
 	df_rel_entries = df[df['TIMESTAMP']==input_t]
 	df_rel_entries.set_index('RANK', inplace=True)
@@ -81,6 +103,6 @@ def update_output_div(input_t):
 													for j in range(1, 11)]
 											)
 	return(list_of_headlines)
-'''
+
 if __name__ == '__main__':
 	app.run_server(debug=True)
