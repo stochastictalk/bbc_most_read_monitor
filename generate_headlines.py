62908cc70e0c2b8cd381e6a2e0a3807b4027de6d
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-22 22:26:19 +0100

    Create 'synthetic_headlines' relation, write null synthetic data to it

--- generate_headlines.py
+++ generate_headlines.py
@@ -1,7 +1,8 @@
 # -*- utf-8 -*-
 
-from datetime import date
+from datetime import date, datetime, timedelta
 import utilities as utils
+from itertools import accumulate
 
 def write_synthetic_headlines(start_date: date, end_date: date, period: int):
     ''' Creates PostgreSQL relation 'synthetic_headlines' containing fields
@@ -18,18 +19,44 @@ def write_synthetic_headlines(start_date: date, end_date: date, period: int):
     '''
 
     # 1. Compute the number of headlines to generate.
-    n_days = (start_date - end_date).days
+    start_date = datetime(start_date.year, start_date.month,
+                          start_date.day, 0, 0)
+    end_date = datetime(end_date.year, end_date.month,
+                        end_date.day, 0, 0)
+    n_days = (end_date - start_date).days
     minutes_in_day = 24*60
     n_periods = int((n_days*minutes_in_day)/period)
     n_capture_events = n_periods + 1 # |----|----|, get it?
     n_headlines_generated = n_capture_events*10
+    time_delta = timedelta(minutes=period)
 
-    # 2. Sample the headlines.
+    # 2. Sample the headlines, urls, ranks, and timestamps
     headlines = sample_synthetic_headlines(n_headlines_generated)
+    urls = sample_synthetic_urls(n_headlines_generated)
+    ranks = (1 + (j % 10) for j in range(n_headlines_generated))
+    timestamps = (int((start_date + j*time_delta).timestamp())
+                                        for j in range(n_capture_events)
+                                                            for k in range(10))
+    # nb interior iterator changes slowest, exterior iterator  changes fastest
 
     # 3. Associate the headlines with a rank, URL, and timestamp.
-
+    h_u_r_ts = zip(headlines, urls, ranks, timestamps)
+    iter_of_headline_dcts = ({'headline':v[0], 'url':v[1],
+                              'rank':v[2], 'timestamp':v[3]} for v in h_u_r_ts)
 
     # 4. Write the resulting data set to a PostgreSQL relation.
+    #for val in iter_of_headline_dcts:
+    #    print(val)
     relation_name = 'synthetic_headlines'
-    utils.write_to_sql(lits_of_headline_dcts)
+    utils.write_to_sql(iter_of_headline_dcts, relation_name)
+
+def sample_synthetic_headlines(n: int):
+    return ('hello world {}'.format(k+1) for k in range(n))
+
+def sample_synthetic_urls(n: int):
+    return ('/news-england-hello-world-{}'.format(k+1) for k in range(n))
+
+start_date = date(2020, 10, 22)
+end_date = date(2020, 10, 23)
+period = 5 # minutes
+write_synthetic_headlines(start_date, end_date, period)
